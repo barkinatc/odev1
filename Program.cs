c669f38cbf0d042ba649f214e8b08c2de18e27bf
Barkın Atıcı <brknatici@hotmail.com>
2022-07-08 19:24:09 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int size;
+
+Console.Write("Bir sayı giriniz: ");
+size = Convert.ToInt32(Console.ReadLine());
+
+int[] dizi = new int[size];
+
+for (int i = 0; i < dizi.Length; i++)
+{
+    Console.WriteLine(i+1 + "." + " sayıyı giriniz: ");
+    dizi[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Clear();
+
+foreach (int x in dizi)
+{
+    if (x % 2==0)
+        Console.WriteLine(x);
+}
